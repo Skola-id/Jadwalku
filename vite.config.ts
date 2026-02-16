b9e5a96ad63b1b6df461f4df9fb7a4ab3ea3cce6
alexanderwidjanarko <alexanderwidjanarko180@gmail.com>
2026-02-16 13:01:07 +0700

    update pesan

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-  base: "/jadwalku/",
+  base: "/Jadwalku/",
   server: {
     port: 3000,
   },
